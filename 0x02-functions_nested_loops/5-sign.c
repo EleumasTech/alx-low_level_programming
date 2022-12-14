68f7f1e51bd2b3f2e5e592beca5ff6292cdc1b3d
EleumasTech <fedieleumassam@gmail.com>
2022-12-14 00:26:52 -0800

    Task 5 to 9

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,24 +3,24 @@
 /**
  * print_sign -function that prints the sign of a number
  * @c: The input number to check
- * Return: int.
+ * Return: 0 or 1
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(43);
+		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-		_putchar(45);
-		return (-1);
+		_putchar('0');
+		return (0);
 	}
 	else
 	{
-		_putchar(48);
-		return (0);
+		_putchar('-');
+		return (-1);
 	}
 }

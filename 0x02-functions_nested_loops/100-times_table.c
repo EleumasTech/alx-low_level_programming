6ce17848dd63d39cf3dbae1f682c55757f611782
EleumasTech <fedieleumassam@gmail.com>
2022-12-15 15:15:29 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,7 +4,7 @@
 /**
  * print_times_table - prints time table with provided number
  * @n: the provided value by the user
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 
 void print_times_table(int n)
